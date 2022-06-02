516738d004a1072548050251c84d886008cbfae5
leenagyoung <2000skrud@naver.com>
2022-06-02 21:46:58 +0900

    Add subject information(4)

---
+++ 컴퓨터공학과.js
@@ -0,0 +1,22 @@
+var request = require('request');
+var options = {
+  'method': 'GET',
+  'url': 'https://sugang.khu.ac.kr/core?attribute=lectListJson&lang=ko&loginYn=N&menu=1&p_major=A10627&p_year=2022&p_term=10&initYn=Y',
+  'headers': {
+    'Cookie': 'JSESSIONID=0hka3HzdSAOOvWw17F0W3OixVWW5MKzJg31nZrYU3yjMlmIv7GF9vasjvDsYwTAe.amV1c19kb21haW4vV0hfU3VnYW5nMQ==; WMONID=R84FN19dbP4'
+  },
+  formData: {
+    'attribute': 'lectListJson',
+    'lang': 'ko',
+    'loginYn': 'N',
+    'menu': '1',
+    'p_major': 'A10627',
+    'p_year': '2022',
+    'p_term': '10',
+    'initYn': 'Y'
+  }
+};
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
+});
