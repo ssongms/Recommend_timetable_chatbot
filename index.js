29d51948c6390a098236b27a55c520a0d9eb1729
송민석 <songms0909@khu.ac.kr>
2022-06-03 16:00:52 +0900

    Make DB Shcema

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const config =require('./config')
 const mongoose = require("mongoose");
-const connect = mongoose
-  .connect(config.url, {
+const connect = mongoose.connect(config.url,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB Connected ..."))
-  .catch((err) => console.log(err));
+  .then(() => console.log("DB : Succesfully Connected"))
+  .catch((err) => console.log(err.message));
+
+
+  const TimeSchema = new mongoose.Schema({
+    time: String,
+    prof: String,
+    credit: Number,
+  });
+
+  const SubSchema = new mongoose.Schema({
+    campus: String,
+    college: String,
+    department: String,
+    grade: String,
+    subject: [TimeSchema],
+  });
+
+  const Timeinfo = mongoose.model('Timeinfo', TimeSchema);
+  const Subinfo = mongoose.model('Subinfo', SubSchema);
+
+  module.exports = {Timeinfo, Subinfo};
