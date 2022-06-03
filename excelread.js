4fa15bbbdd20b1bcc49d1e8e2874b0d020833d2c
byeongmin kwak <bqudmals@naver.com>
2022-06-03 16:01:01 +0900

    Update excelread.js

--- excelread.js
+++ excelread.js
@@ -1,29 +1,62 @@
-// @breif xlsx 모듈추출
-
 const xlsx = require( "xlsx" );
+// 엑셀 파일을 가져오기
 
-// @files 엑셀 파일을 가져온다.
+const excelFile = xlsx.readFile( "컴퓨터공학과 권장이수 가이드라인.xlsx");
+// 엑셀 파일의 첫번째 시트의 정보 추출
 
-const excelFile = xlsx.readFile( "컴퓨터공학과 권장이수 가이드라인.xlsx" );
-// @breif 엑셀 파일의 첫번째 시트의 정보를 추출
+const sheetName = excelFile.SheetNames[0];
+const firstSheet = excelFile.Sheets[sheetName];
+const jsonData = xlsx.utils.sheet_to_json(firstSheet);
 
-const sheetName = excelFile.SheetNames[0];          // @details 첫번째 시트 정보 추출
-const firstSheet = excelFile.Sheets[sheetName];       // @details 시트의 제목 추출
+var first_first = [];
+var first_second = [];
+var second_first = [];
+var second_second = [];
+var third_first = [];
+var third_second = [];
+var fourth_first = [];
+var fourth_second = [];
 
-// @details 엑셀 파일의 첫번째 시트를 읽어온다.
+function jason_to_arry() {
+  for (var data in jsonData){
+    first_first.push(jsonData[data]['1학년 1학기']);
+    first_second.push(jsonData[data]['1학년 2학기']);
+    second_first.push(jsonData[data]['2학년 1학기']);
+    second_second.push(jsonData[data]['2학년 2학기']);
+    third_first.push(jsonData[data]['3학년 1학기']);
+    third_second.push(jsonData[data]['3학년 2학기']);
+    fourth_first.push(jsonData[data]['4학년 1학기']);
+    fourth_second.push(jsonData[data]['4학년 2학기']);
+  }
+}
+// jason -> array로 변경
+function remove_null() {
+first_first = first_first.filter(function(item) {
+  return item !== null && item !== undefined && item !== '';
+});
+first_second = first_second.filter(function(item) {
+    return item !== null && item !== undefined && item !== '';
+  });
+second_first = second_first.filter(function(item) {
+    return item !== null && item !== undefined && item !== '';
+});
+second_second = second_second.filter(function(item) {
+    return item !== null && item !== undefined && item !== '';
+  });
+third_first = third_first.filter(function(item) {
+    return item !== null && item !== undefined && item !== '';
+  });
+third_second = third_second.filter(function(item) {
+    return item !== null && item !== undefined && item !== '';
+  });
+fourth_first = fourth_first.filter(function(item) {
+    return item !== null && item !== undefined && item !== '';
+  });
+fourth_second = fourth_second.filter(function(item) {
+    return item !== null && item !== undefined && item !== '';
+  });
+}
+// null 값 제거
 
-const jsonData = xlsx.utils.sheet_to_json( firstSheet, { defval : "" } );
-console.log( jsonData );
-
-var ary = [];
-
-const xlsxFile = require('read-excel-file/node'); xlsxFile('컴퓨터공학과 권장이수 가이드라인.xlsx').then((rows) => { 
-    //console.log(rows); console.table(rows);
-     for (var i in rows) {
-        ary.push(rows[i]);
-        
-       
-    }
-    //console.log(ary)
-    })
-console.log(ary);
+jason_to_arry()
+remove_null()
