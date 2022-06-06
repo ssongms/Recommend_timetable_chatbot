cfc987302e996b1a8390800c38d1be6830dbef1c
송민석 <songms0909@khu.ac.kr>
2022-06-06 16:29:10 +0900

    Not modify

--- excelread.js
+++ excelread.js
@@ -1,13 +1,12 @@
 const xlsx = require( "xlsx" );
 // 엑셀 파일을 가져오기
 
-const excelFile = xlsx.readFile( "컴퓨터공학과 권장이수 가이드라인.xlsx");
+const excelFile = xlsx.readFile("./GuideLine/컴퓨터공학과 권장이수 가이드라인.xlsx");
 // 엑셀 파일의 첫번째 시트의 정보 추출
 
 const sheetName = excelFile.SheetNames[0];
 const firstSheet = excelFile.Sheets[sheetName];
 const jsonData = xlsx.utils.sheet_to_json(firstSheet);
-
 var first_first = [];
 var first_second = [];
 var second_first = [];
@@ -58,5 +57,8 @@ fourth_second = fourth_second.filter(function(item) {
 }
 // null 값 제거
 
+
 jason_to_arry()
 remove_null()
+
+console.log(first_first);
